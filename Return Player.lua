38e9a78843397118bcdc2e02349b4476f9d687b5
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-06-09 16:35:04 -0400

    Create Return Player.lua

---
+++ Return Player.lua
@@ -0,0 +1,12 @@
+-- // Get Target
+local ReturnTarget = function(Player)
+    local Players = game:GetService('Players'):GetPlayers()
+
+    for _, v in next, Players do
+        if v and v.Name:lower():sub(1, #Player):match(Player:lower()) then
+            return v
+        end
+    end
+    
+    return nil
+end
