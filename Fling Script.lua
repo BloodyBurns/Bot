b3a7a696a6a2692c0d59a201a87fcb79f749e244
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-06-10 21:04:32 -0400

    Create Fling Script.lua

---
+++ Fling Script.lua
@@ -0,0 +1,27 @@
+-- // Fling Script
+local Fling = function()
+    local plr = game:GetService("Players").LocalPlayer
+
+    game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            plr.Character.Humanoid:SetStateEnabled("Seated", false)
+            plr.Character.Animate.Disabled = true
+            plr.Character.Humanoid.Sit = true
+        end)
+    end)
+
+    game:GetService("RunService").Heartbeat:Connect(function()
+        pcall(function()
+            plr.Character:SetPrimaryPartCFrame(
+                (_G.FlingBotTarget.Character:FindFirstChild("HumanoidRootPart").CFrame or
+                    _G.FlingBotTarget.Character:FindFirstChildWhichIsA("BasePart").CFrame))
+                    
+            plr.Character.HumanoidRootPart.CFrame =
+                (_G.FlingBotTarget.Character:FindFirstChild("HumanoidRootPart").CFrame or
+                _G.FlingBotTarget.Character:FindFirstChildWhichIsA("BasePart").CFrame)
+
+            plr.Character.HumanoidRootPart.Velocity = Vector3.new(0, -10e10, 10e10)
+            plr.Character.HumanoidRootPart.RotVelocity = Vector3.new(0, -10e10, 10e10)
+        end)
+    end)
+end
