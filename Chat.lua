d82d897d860cbbcf38b353d5d4427e82174af307
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-06-10 21:15:18 -0400

    Update Chat.lua

--- Chat.lua
+++ Chat.lua
@@ -1,12 +1,6 @@
 -- // Chat
 local SayMessage = function(Message)
-    if Message then
-        if type(Message) = "string" then
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(tostring(Message), "All")
-        elseif type(Message) == "table" then
-            for _, v in next, Message do
-                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(tostring(v), "All")
-            end
-        end
+    if Message and type(Message) = "string" then
+        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(tostring(Message), "All")
     end
 end
