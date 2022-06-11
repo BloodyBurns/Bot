aeec9acdb8ba76df61e07f2ee15d6b365ba1b65b
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-06-10 21:01:25 -0400

    Update Return Player.lua

--- Return Player.lua
+++ Return Player.lua
@@ -1,10 +1,12 @@
 -- // Get Target
 local ReturnTarget = function(Player)
-    local Players = game:GetService('Players'):GetPlayers()
+    if type(Player) == 'string' then
+        local Players = game:GetService('Players'):GetPlayers()
 
-    for _, v in next, Players do
-        if v and v.Name:lower():sub(1, #Player):match(Player:lower()) then
-            return v
+        for _, v in next, Players do
+            if v and v.Name:lower():sub(1, #Player):match(Player:lower()) then
+                return v
+            end
         end
     end
     
