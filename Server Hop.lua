829a53de7c96453d3fc7d45223dff4004124dc6f
BloodyBurns <83672765+BloodyBurns@users.noreply.github.com>
2022-06-19 12:17:51 -0400

    Update Server Hop.lua

--- Server Hop.lua
+++ Server Hop.lua
@@ -2,7 +2,7 @@ local Hop = function()
   local IDs = {}
 	local api = "https://games.roblox.com/v1/games/PlaceId/servers/Public?sortOrder=Asc&limit=100"
 
-	for _, v in next, HttpService:JSONDecode(game:HttpGetAsync(api:gsub('PlaceId', game.PlaceId))).data do
+	for _, v in next, game:GetService('HttpService'):JSONDecode(game:HttpGetAsync(api:gsub('PlaceId', game.PlaceId))).data do
 		table.insert(IDs, v.id)
 	end
 
